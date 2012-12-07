ac40bab3b6e8d0f60054a882d75c5b319d2fcca4
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-12-07 11:36:03 +0100

    'hdist unpack' CLI command

--- hashdist/cmd/source_cache_cmd.py
+++ hashdist/cmd/source_cache_cmd.py
@@ -9,27 +9,17 @@ from ..source_cache import SourceCache
 
 class FetchGit(object):
     """
-    Put files in/download files to the source store
+    Fetch git sources to the source cache
 
-    The idea of the source store is to help download source code from
-    the net, or "upload" files from the local disk. Items in the store
-    are identified with a cryptographic hash.
+    Example::
 
-    Examples::
-
-        $ hdist fetch http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
-        Downloading... progress indicator ...
-        Done
-        targz:mheIiqyFVX61qnGc53ZhR-uqVsY
-
-        $ hdist fetch git://github.com/numpy/numpy.git master
+        $ hdist fetchgit git://github.com/numpy/numpy.git master
         Fetching ...
         Done
         git:c5ccca92c5f136833ad85614feb2aa4f5bd8b7c3
 
     One can then unpack results later only by using the keys::
 
-        $ hdist unpack targz:mheIiqyFVX61qnGc53ZhR-uqVsY src/python
         $ hdist unpack git:c5ccca92c5f136833ad85614feb2aa4f5bd8b7c3 numpy
     
     """
@@ -46,6 +36,31 @@ class FetchGit(object):
         sys.stderr.write('\n')
         sys.stdout.write('%s\n' % key)
 
-
-
 register_subcommand(FetchGit)
+
+
+class Unpack(object):
+    """
+    Unpacks sources that are stored in the source cache to a local directory
+
+    E.g.,
+
+    ::
+    
+        $ hdist unpack targz:mheIiqyFVX61qnGc53ZhR-uqVsY src/python
+        $ hdist unpack git:c5ccca92c5f136833ad85614feb2aa4f5bd8b7c3 numpy
+
+    """
+
+    @staticmethod
+    def setup(ap):
+        ap.add_argument('key', help='Key of stored sources previously returned from a fetch command')
+        ap.add_argument('target', help='Where to unpack sources')
+
+    @staticmethod
+    def run(ctx, args):
+        store = SourceCache.create_from_config(ctx.config)
+        store.unpack(args.key, args.target)
+
+register_subcommand(Unpack)
+
