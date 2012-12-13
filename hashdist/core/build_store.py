37fd121e1a633baa81fcaf47f48cba02ef4074d4
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-12-13 21:19:10 +0100

    DOC more build store docs

--- hashdist/core/build_store.py
+++ hashdist/core/build_store.py
@@ -1,3 +1,172 @@
+"""
+:mod:`hashdist.core.build_store` --- Build artifact store
+=========================================================
+
+Principles
+----------
+
+The build store is the very core of Hashdist: Producing build artifacts
+identified by hash-IDs. It's important to have a clear picture of just
+what the build store is responsible for and not.
+
+Nix takes a pure approach where an artifact hash is guaranteed to
+identify the resulting binaries (up to anything inherently random in
+the build process, like garbage left by compilers). In contrast,
+Hashdist takes a much more lenient approach where the strictness is
+configurable. The primary goal of Hashdist is to make life simpler
+by reliably triggering rebuilds when software components are updated,
+not total control of the environment (in which case Nix is likely
+the better option).
+
+The *only* concern of the build store is managing the result of a
+build.  So the declared dependencies in the build-spec are not the
+same as "package dependencies" found in higher-level distributions;
+for instance, if a pure Python package has a NumPy dependency, this
+should not be declared in the build-spec because NumPy is not needed
+during the build; indeed, the installation can happen in
+parallel. Assembing artifacts together in a usable run-time system is
+the job of :mod:`hashdist.core.profile`.
+
+
+Artifact IDs
+------------
+
+A Hashdist artifact ID has the form ``name/version/hash``, e.g.,
+``zlib/1.2.7/fXHu+8dcqmREfXaz+ixMkh2LQbvIKlHf+rtl5HEfgmU``.
+
+ * `name` is a descriptive name for the package
+ * `version` describes the specific build in human-friendly terms;
+   this may be a simple version number (``1.2``) or something
+   more descriptive (``1.2-beta-intel-openblas-avx``). For simplicity
+   we require this to always be present; by convention set it to ``n`` for
+   "does not apply" or ``dev`` for "not released yet".
+ * `hash` is a secure sha-256 hash of the build specification (43 characters)
+
+All explicit references to build artifacts happens by using the full
+three-part ID.
+
+For the artifact paths on disk, a shortened form (4-char hash) is used
+to make things more friendly to the human user. If there is a
+collision, the length is simply increased for the one that comes
+later. Thus, the example above could be stored on disk as
+``~/.hdist/opt/zlib/1.2.7/fXHu``, or ``~/.hdist/opt/zlib/1.2.7/fXHu+``
+in the (rather unlikely) case of a collision. There is a symlink
+from the full ID to the shortened form. See also Discussion below.
+
+Build specifications and inferring artifact IDs
+-----------------------------------------------
+
+The fundamental object of the build store is the JSON build specification.
+If you know the build spec, you know the artifact ID, since the former
+is essentially the hash of the latter. The key to achieving this is that
+both `dependencies` and `sources` are specified in terms of their hashes.
+
+.. code-block:: python
+
+    {
+        "name" : "<name of piece of software>",
+        "version" : "<human-readable description what makes this build special>",
+        "dependencies" : {
+            "unix" : "virtual:unix",
+            "bash" : "virtual:bash",
+            "zlib" : "zlib/1.2.7/fXHu+8dcqmREfXaz+ixMkh2LQbvIKlHf+rtl5HEfgmU",
+            "gcc" : "gcc/host-4.6.3/q0VSL7JmzH1P17meqITYc4kMbnIjIexrWPdlAlqPn3s",
+         },
+         "sources" : [
+             {"key": "git:c5ccca92c5f136833ad85614feb2aa4f5bd8b7c3"},
+             {"key": "tar.bz2:RB1JbykVljxdvL07mN60y9V9BVCruWRky2FpK2QCCow", "target": "sources", "strip": 1}
+             {"key": "files:5fcANXHsmjPpukSffBZF913JEnMwzcCoysn-RZEX7cM"}
+         ]
+         "command" : ["$bash/bin/bash", "build.sh"]
+    }
+
+Field documentation.
+
+**name**:
+    See previous section
+
+**version**:
+    See previous section
+
+**dependencies**:
+    The dependencies needed for the  *build*, remember that after the
+    artifact is built these have no effect. The effect of these are:
+
+    * The build can not be performed if the dependencies are not present
+      in the build store
+    * ``$PATH`` for execution contains the ``bin`` subdirectory
+      of each dependency (one can always override the PATH in a script)
+    * Environment variables are set up as follows (using the ``zlib``
+      dependency as an example):
+
+      * ``$zlib``: Absolute path to ``zlib`` build artifact
+      * ``$zlib_id``: Build artifact ID
+      * ``$zlib_relpath``: Relative path from ``$PREFIX`` to the zlib artifact
+
+**sources**:
+    Unpacked into the temporary build directory. The optional ``target`` parameter
+    gives a directory they should be extracted to (default: ``"."``). The ``strip``
+    parameter (only applies to tarballs) acts like the
+    `tar` ``--strip-components`` flag.
+
+Build execution
+---------------
+
+ #. A temporary build directory is created (current directory and ``$BUILD_DIR``)
+
+ #. A directory for the build results is created, available in the ``$PREFIX``
+    environment variable.
+
+ #. The build spec is serialized as ``build.json`` in both the directories
+    above.
+
+ #. The sources listed are unpacked
+
+ #. Environment is set up (see build spec section) and the command
+    given executed. All output is piped to ``$BUILD_DIR/build.log``.
+
+ #. On success, logfile is copied to ``$PREFIX`` as well. The build directory
+    may be removed depending on configuration.
+
+
+
+
+Discussion
+----------
+
+Safety of the shortened IDs
+'''''''''''''''''''''''''''
+
+Hashdist will never use these to resolve build artifacts, so collision
+problems come in two forms:
+
+First, automatically finding the list of run-time dependencies from
+the build dependencies. In this case one scans the artifact directory
+only for the build dependencies (less than hundred). It then makes
+sense to consider the chance of finding one exact string
+``aaa/0.0/ZXa3`` in a random stream of 8-bit bytes, which helps
+collision strength a lot, with chance "per byte" of
+collision on the order :math:`2^{-(8 \cdot 12)}=2^{-96}`
+for this minimal example.
+
+If this is deemed a problem (the above is too optimistice), one can
+also scan for "duplicates" (other artifacts where longer hashes
+were chosen, since we know these).
+
+The other problem can be future support for binary distribution of
+build artifacts, where you get pre-built artifacts which have links to
+other artifacts embedded, and artifacts from multiple sources may
+collide. In this case it makes sense to increase the hash lengths a
+bit since the birthday effect comes into play and since one only has 6
+bits per byte. However, the downloaded builds presumably will contain
+the full IDs, and so on can check if there is a conflict and give an
+explicit error.
+
+Reference
+---------
+
+"""
+
 import os
 from os.path import join as pjoin
 import tempfile
