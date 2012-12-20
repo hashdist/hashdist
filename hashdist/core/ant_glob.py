28da4f8028ed86f419995d290c0eb10fb080f404
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-12-20 15:24:06 +0100

    basic ant_glob

---
+++ hashdist/core/ant_glob.py
@@ -0,0 +1,80 @@
+"""
+:mod:`hashdist.core.ant_glob` -- ant-inspired globbing
+======================================================
+
+
+"""
+
+import os
+import re
+from os.path import join as pjoin
+
+from glob import glob
+
+def glob_files(pattern, cwd='.'):
+    """
+    Generator that iterates over files matching the pattern.
+
+    The syntax is ant-glob-inspired but currently only a small subset
+    is implemented.
+
+    Examples::
+
+        *.txt         # matches "a.txt", "b.txt"
+        foo/**/bar    # matches "foo/bar" and "foo/a/b/c/bar"
+        foo*/**/*.bin # matches "foo/bar.bin", "foo/a/b/c/bar.bin", "foo3/a.bin"
+
+    Illegal patterns::
+
+        foo/**.bin  # '**' can only match 0 or more entire directories
+        
+
+    Issues
+    ------
+
+     * Does not support escaped / characters
+     * Does not support any regular globbing-patterns besides *
+
+    Parameters
+    ----------
+
+    pattern : str or list
+        Glob pattern as described above. If a str, will be split by /;
+        if a list, each item is a path component.
+    
+    """
+    if isinstance(pattern, (str, unicode)):
+        parts = pattern.split('/')
+    else:
+        parts = list(pattern)
+
+    if len(parts) == 0:
+        raise ValueError('empty glob pattern')
+    
+    part = parts[0]
+    is_last = len(parts) == 1
+    if part == '**':
+        if is_last:
+            raise ValueError('does not make sense with ** at end of pattern with '
+                             'glob_files')
+        for dirpath, dirnames, filenames in os.walk(cwd):
+            for x in glob_files(parts[1:], dirpath):
+                yield x
+    elif '**' in part:
+        raise NotImplementedError('mixing ** and other strings in same path component not supported')
+    else:
+        part = re.escape(part)
+        part = part.replace('\\*', '.*')
+        part_re = re.compile(part)
+        if is_last:
+            for name in os.listdir(cwd):
+                path = pjoin(cwd, name)
+                if part_re.match(name) and os.path.isfile(path):
+                    yield path
+        else:
+            for name in os.listdir(cwd):
+                path = pjoin(cwd, name)
+                if part_re.match(name) and os.path.isdir(path):
+                    for x in glob_files(parts[1:], path):
+                        yield x
+        
---
+++ hashdist/core/test/test_ant_glob.py
@@ -0,0 +1,38 @@
+from .utils import temp_working_dir
+
+from ..ant_glob import glob_files
+
+import os
+from os import makedirs
+
+def makefiles(lst):
+    for x in lst:
+        x = x.strip()
+        dirname, basename = os.path.split(x)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        with file(x, 'w') as f:
+            pass
+
+def test_basic():
+    with temp_working_dir() as d:
+        makefiles('a0/b0/c0/d0.txt a0/b0/c0/d1.txt a0/b1/c1/d0.txt a0/b.txt'.split())
+        #os.system('find')
+        #print '===='
+
+        def check(expected, pattern):
+            # check relative
+            assert sorted(expected) == sorted(glob_files(pattern))
+            # check absolute
+            abs_expected = [os.path.realpath(e) for e in expected]
+            with temp_working_dir():
+                assert sorted(abs_expected) == sorted(glob_files(pattern, d))
+        
+        yield (check, ['./a0/b0/c0/d0.txt'],
+               'a0/b0/c0/d0.txt')
+        yield (check, ['./a0/b1/c1/d0.txt', './a0/b0/c0/d0.txt'],
+              'a0/**/d0.txt')
+        yield (check, ['./a0/b.txt', './a0/b1/c1/d0.txt', './a0/b0/c0/d0.txt', './a0/b0/c0/d1.txt'],
+              'a0/**/*.txt')
+        yield (check, ['./a0/b0/c0/d0.txt', './a0/b0/c0/d1.txt'],
+              '**/b0/**/*.txt')
