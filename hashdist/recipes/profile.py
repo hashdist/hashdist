a69c33611c4d8a74bcb007ef8f43a3dce60764c8
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2012-12-21 14:09:34 +0100

    fixup: forgot to commit recipes/profile.py

---
+++ hashdist/recipes/profile.py
@@ -0,0 +1,22 @@
+from .recipes import Recipe
+
+class Profile(Recipe):
+    def __init__(self, dependency_lst):
+        if any([dep.is_virtual for dep in dependency_lst]):
+            raise ValueError("should not put virtual artifacts in a profile")
+        dependencies = dict((dep.name, dep) for dep in dependency_lst)
+        Recipe.__init__(self, "profile", "n", dependencies=dependencies)
+
+    def get_commands(self):
+        return [["hdist", "create-profile", "--key=parameters/profile", "build.json", "$ARTIFACT"]]
+
+    def get_dependencies_spec(self):
+        # we don't need the dependencies in the profile build environment...
+        return []
+
+    def get_parameters(self):
+        profile = []
+        for ref, dep in self.dependencies.iteritems():
+            before = [bef.get_artifact_id() for bef in dep.before]
+            profile.append({"id": dep.get_artifact_id(), "before": before})
+        return {"profile": profile}
