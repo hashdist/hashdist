2feec3da62636bebc3b7f9a77dd2b95eca309c20
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2013-01-07 15:40:10 +0100

    DiskCache: add a memory cache layer

--- hashdist/core/cache.py
+++ hashdist/core/cache.py
@@ -18,11 +18,15 @@ import errno
 from .hasher import Hasher
 from .fileutils import silent_makedirs
 
-NOT_PRESENT = object()
+_RAISE = object()
 
 class DiskCache(object):
+    """
+    Key/value store to cache th
+    """
     def __init__(self, cache_path):
         self.cache_path = cache_path
+        self.memory_cache = {}
     
     @staticmethod
     def create_from_config(config, logger):
@@ -30,6 +34,11 @@ class DiskCache(object):
         """
         return Cache(config.get_path('general', 'cache'))
 
+    def _as_domain(self, domain):
+        if not isinstance(domain, str):
+            domain = '%s.%s' % (domain.__module__, domain.__name__)
+        return domain
+    
     def _get_obj_filename(self, domain, key):
         h = Hasher()
         h.update((domain, key))
@@ -42,21 +51,37 @@ class DiskCache(object):
         This should be atomic and race-safe, provided that everybody who
         does a put puts the same value.
 
+        .. warning::
+
+            `value` should be immutable or at least never modified,
+            since the same instance can be retreived from the memory
+            cache
+
         Parameters
         ----------
 
-        domain : str
-            Should be a string that is combined with the key to perform
-            the lookup; this is present to enforce that keyspaces don't
-            collide
+        domain : type or str
+            Should be a type or a string that is combined with the key
+            to perform the lookup; this is present to enforce that
+            keyspaces don't collide. If a type is passed, then its
+            fully qualified name is taken as the domain string.
 
         key : object
             Object that is hashable with :cls:`Hasher`.
 
         value : object
-            Pickleable object
+            Pickleable object.
         """
+        domain = self._as_domain(domain)
         obj_filename = self._get_obj_filename(domain, key)
+
+        # memory cache
+        if obj_filename in self.memory_cache:
+            # already stored from this same cache object; don't bother with writing
+            # to disk
+            return
+        self.memory_cache[obj_filename] = value
+        
         # dump to temporary file + atomic rename
         obj_dir = os.path.dirname(obj_filename)
         silent_makedirs(obj_dir)
@@ -69,31 +94,50 @@ class DiskCache(object):
             os.unlink(temp_filename)
             raise
 
-    def get(self, domain, key, default=NOT_PRESENT):
+    def get(self, domain, key, default=_RAISE):
         """Looks up value from store
 
         Parameters
         ----------
 
-        domain : str
-            Should be a string that is combined with the key to perform
-            the lookup; this is present to enforce that keyspaces don't
-            collide
+        domain : type or str
+            Should be a type or a string that is combined with the key
+            to perform the lookup; this is present to enforce that
+            keyspaces don't collide. If a type is passed, then its
+            fully qualified name is taken as the domain string.
 
         key : object
             Object that is hashable with :cls:`Hasher`.
 
         default : object (optional)
-            Object to return if lookup fails; defaults to `NOT_PRESENT`.
+            Object to return if lookup fails. If not present, a KeyError
+            is raised.
         """
+        domain = self._as_domain(domain)
         obj_filename = self._get_obj_filename(domain, key)
-
         try:
-            f = file(obj_filename)
-        except IOError, e:
-            if e.errno != errno.ENOENT:
-                raise
+            x = self.memory_cache[obj_filename]
+        except KeyError:
+            try:
+                f = file(obj_filename)
+            except IOError, e:
+                if e.errno != errno.ENOENT:
+                    raise
+                if default is not _RAISE:
+                    return default
+                else:
+                    raise KeyError('Cannot find object in key-domain "%s" that hashes to %s' %
+                                   (domain, obj_filename))
+            with f:
+                x = self.memory_cache[obj_filename] = pickle.load(f)
+        return x
+
+class NullCache(object):
+    def put(self, domain, key, value):
+        pass
+
+    def get(self, domain, key, default=_RAISE):
+        if default is _RAISE:
+            raise KeyError('keys are never found in the NullCache')
+        else:
             return default
-        
-        with f:
-            return pickle.load(f)
--- hashdist/core/test/test_cache.py
+++ hashdist/core/test/test_cache.py
@@ -1,9 +1,12 @@
 import functools
 import os
 import glob
+from os.path import join as pjoin
+
+from nose.tools import assert_raises
 
 from .utils import temp_dir
-from ..cache import DiskCache, NOT_PRESENT
+from ..cache import DiskCache
 
 def fixture():
     def decorator(func):
@@ -19,14 +22,27 @@ def test_basic(cache, tmpdir):
     cache.put('foo', 'bar', 1)
     cache.put('foo', 'baz', 2)
     assert cache.get('foo', 'bar') == 1
+    assert DiskCache(tmpdir).get('foo', 'bar') == 1
+
     assert cache.get('foo', 'baz') == 2
  
     cache.put('foo2', 'bar', 2)
     assert cache.get('foo2', 'bar') == 2
     assert cache.get('foo', 'bar') == 1
 
-    assert cache.get('nonexisting', 'bar') is NOT_PRESENT
+    with assert_raises(KeyError):
+        cache.get('nonexisting', 'bar')
     assert cache.get('nonexisting', 'bar', 'default') == 'default'
+
+@fixture()
+def test_memory_caching(cache, tempdir):
+    # We can retreive even if we remove the backing file, as long as cache is
+    # not destructed...
+    cache.put('foo', 'bar', 1)
+    assert len(cache.memory_cache) == 1
+    os.unlink(glob.glob(pjoin(tempdir, '*', '*'))[0])
+    assert cache.get('foo', 'bar') == 1
+    assert DiskCache(tempdir).get('foo', 'bar', -1) == -1
     
 @fixture()
 def test_unpickleable(cache, tmpdir):
