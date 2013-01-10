a3c92d6f31bcee4d0ac21bef7a455ccb8f7c1cd7
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2013-01-10 10:19:39 +0100

    tests for fileutils

---
+++ hashdist/core/test/test_fileutils.py
@@ -0,0 +1,27 @@
+import os
+from os.path import join as pjoin
+
+from nose.tools import assert_raises
+
+from .utils import temp_dir
+from .. import fileutils
+
+def test_rmtree_up_to():
+    with temp_dir() as d:
+        # Incomplete removal
+        os.makedirs(pjoin(d, 'a', 'x', 'A', '2'))
+        os.makedirs(pjoin(d, 'a', 'x', 'B', '2'))
+        fileutils.rmtree_up_to(pjoin(d, 'a', 'x', 'A', '2'), d)
+        assert ['B'] == os.listdir(pjoin(d, 'a', 'x'))
+
+        # Invalid parent parameter
+        with assert_raises(ValueError):
+            fileutils.rmtree_up_to(pjoin(d, 'a', 'x', 'B'), '/nonexisting')
+
+        # Complete removal -- do not actually remove the parent
+        fileutils.rmtree_up_to(pjoin(d, 'a', 'x', 'B', '2'), d)
+        assert os.path.exists(d)
+
+        # Parent is exclusive
+        fileutils.rmtree_up_to(d, d)
+        assert os.path.exists(d)
