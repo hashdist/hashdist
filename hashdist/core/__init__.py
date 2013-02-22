7e2f52df06b7c1326416995be0f5f3374bc94e39
Ondrej Certik <ondrej.certik@gmail.com>
2013-02-22 12:54:05 -0800

    Fixes failures introduced in 6b2dd77

--- hashdist/core/__init__.py
+++ hashdist/core/__init__.py
@@ -5,7 +5,7 @@ from .source_cache import (SourceCache, supported_source_archive_types,
                            single_file_key, hit_pack)
 from .build_store import (BuildStore, BuildSpec, shorten_artifact_id)
 from .profile import make_profile
-from .hdist_recipe import hdist_cli_build_spec, HDIST_CLI_ARTIFACT_NAME, HDIST_CLI_ARTIFACT_VERSION
+from .hit_recipe import hit_cli_build_spec, HIT_CLI_ARTIFACT_NAME, HIT_CLI_ARTIFACT_VERSION
 from .cache import DiskCache, null_cache, cached_method
 from .run_job import InvalidJobSpecError, JobFailedError
 from .fileutils import atomic_symlink
