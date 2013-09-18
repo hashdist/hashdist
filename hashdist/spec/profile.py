a3f63ea16a99b741ec4c4a277df9491047e2bef7
Dag Sverre Seljebotn <d.s.seljebotn@astro.uio.no>
2013-09-18 15:10:41 +0200

    spec.profile: New, more obvious handling of locations base_dir, packages_dir

--- hashdist/spec/profile.py
+++ hashdist/spec/profile.py
@@ -32,44 +32,80 @@ class FileResolver(object):
         self.children = children
 
 
+class DirectoryRemover(object):
+    """
+    Removes a directory once nothing references this object any longer
+    """
+    def __init__(self, path):
+        self.path = path
+
+    def __del__(self):
+        if self.path:
+            shutil.rmtree(self.path)
+            self.path = None
+
+
 class Profile(object):
     """
-
     Profiles acts as nodes in a tree, with `extends` containing the
     parent profiles (which are child nodes in a DAG).
+
+    Other nodes in the DAG are PackageFiles and BaseFiles.
     """
-    def __init__(self, basedir, doc_name, doc, extends, rm_on_close=False):
-        self.basedir = basedir
-        self.doc_name = doc_name
+    def __init__(self, filename, doc, includes, hold_ref_to=None):
+        self.filename = filename
         self.doc = doc
-        self.extends = extends
-        self.rm_on_close = rm_on_close
+        self.includes = includes
+        self._hold_ref_to = hold_ref_to
         # for now, we require that bases have non-overlapping parameter keys
         self.parameters = {}
-        for base in extends:
+        for base in includes:
             for k, v in base.parameters.iteritems():
                 if k in self.parameters:
                     raise ConflictingProfilesError('two base profiles set same parameter %s' % k)
                 self.parameters[k] = v
         self.parameters.update(doc.get('parameters', {}).get('global', {}))
 
-    def close(self):
-        for base in self.extends:
-            base.close()
-        if self.rm_on_close:
-            shutil.rmtree(self.basedir)
+        d = os.path.dirname(filename)
+        self._packages_dir = os.path.abspath(pjoin(d, doc['packages_dir'])) if 'packages_dir' in doc else None
+        self._base_dir = os.path.abspath(pjoin(d, doc['base_dir'])) if 'base_dir' in doc else None
 
-    def find_file(self, relname):
-        path = pjoin(self.basedir, relname)
-        if not os.path.exists(path):
-            path = None
-            for base in self.extends:
-                path_in_base = base.find_file(relname)
-                if path_in_base is not None:
-                    if path is not None:
-                        raise ConflictingProfilesError('file %s found in two different base profiles' % relname)
-                    path = path_in_base
-        return path
+    def _find_resource_in_includes(self, resource_type, resource_getter_name, resource_name):
+        filename = None
+        for child in self.includes:
+            child_filename = getattr(child, resource_getter_name)(resource_name)
+            if child_filename is not None:
+                if filename is not None:
+                    raise ConflictingProfilesError('%s %s found in sibling included profiles' %
+                                                   (resource_type, resource_name))
+            filename = child_filename
+        return filename
+
+    def find_package_file(self, name):
+        # try pkgs/foo.yaml
+        filename = None
+        if self._packages_dir is not None:
+            filename = pjoin(self._packages_dir, name + '.yaml')
+            if not os.path.exists(filename):
+                # try pkgs/foo/foo.yaml
+                filename = pjoin(self._packages_dir, name, name + '.yaml')
+                if not os.path.exists(filename):
+                    filename = None
+
+        if filename is None:
+            # try included profiles; only allow it to come from one of them
+            filename = self._find_resource_in_includes('package', 'find_package_file', name)
+        return filename
+
+    def find_base_file(self, name):
+        filename = None
+        if self._base_dir is not None:
+            filename = pjoin(self._base_dir, name)
+            if not os.path.exists(filename):
+                filename = None
+        if filename is None:
+            filename = self._find_resource_in_includes('base file', 'find_base_file', name)
+        return filename
 
     def get_python_path(self, path=None):
         """
@@ -132,20 +168,14 @@ class Profile(object):
         return packages
 
     def __repr__(self):
-        return '<Profile %s>' % pjoin(self.basedir, self.doc_name)
+        return '<Profile %s>' % self.filename
 
-    def __enter__(self):
-        return self
 
-    def __exit__(self, type, value, traceback):
-        self.close()
-
-def load_profile(source_cache, include_doc):
+def load_profile(source_cache, include_doc, cwd=None):
     """
     Loads a Profile given an include document fragment, e.g.::
 
-        profile: profile.yaml
-        dir: /path/to/local/directory
+        profile: ../foo/profile.yaml
 
     or::
 
@@ -156,25 +186,33 @@ def load_profile(source_cache, include_doc):
     The load happens recursively, including fetching any remote
     dependencies.
     """
-    profile_rel_file = include_doc['profile']
-    if 'dir' in include_doc:
-        basedir = include_doc['dir']
-        created_basedir = False
-    else:
-        # check out git repo to temporary directory
+    def resolve_path(cwd, p):
+        if not os.path.isabs(p):
+            if cwd is None:
+                cwd = os.getcwd()
+            p = os.path.abspath(pjoin(cwd, p))
+        return p
+    
+    if 'key' in include_doc:
+        # Check out git repo to temporary directory. cwd is relative to checked out root.
         assert len(include_doc['urls']) == 1
-        basedir = tempfile.mkdtemp()
-        created_basedir = True
+        cwd = tempfile.mkdtemp()
+        dir_remover = DirectoryRemover(cwd)
         source_cache.fetch(include_doc['urls'][0], include_doc['key'], 'stack-desc')
-        source_cache.unpack(include_doc['key'], basedir)
-
-
-    assert os.path.isabs(basedir)
-    with open(pjoin(basedir, profile_rel_file)) as f:
-        doc = marked_yaml_load(f)
-    if 'extends' in doc:
-        extends = [load_profile(source_cache, parent_include) for parent_include in doc['extends']]
-        del doc['extends']
+        source_cache.unpack(include_doc['key'], cwd)
     else:
-        extends = []
-    return Profile(basedir, profile_rel_file, doc, extends, rm_on_close=created_basedir)
+        dir_remover = None
+
+    profile = resolve_path(cwd, include_doc['profile'])
+
+    with open(profile) as f:
+        doc = marked_yaml_load(f)
+    if doc is None:
+        doc = {}
+    if 'include' in doc:
+        new_cwd = os.path.dirname(profile)
+        includes = [load_profile(source_cache, include, cwd=new_cwd) for include in doc['include']]
+        del doc['include']
+    else:
+        includes = []
+    return Profile(profile, doc, includes, hold_ref_to=dir_remover)
--- hashdist/spec/tests/test_profile.py
+++ hashdist/spec/tests/test_profile.py
@@ -12,7 +12,23 @@ from ...core.test.test_source_cache import temp_source_cache
 from .. import profile
 
 
+def gitify(dir):
+    with working_directory(dir):
+        subprocess.check_call(['git', 'init'], stdout=open(os.devnull, 'wb'))
+        subprocess.check_call(['git', 'add', '.'], stdout=open(os.devnull, 'wb'))
+        subprocess.check_call(['git', 'commit', '-m', 'Initial commit'], stdout=open(os.devnull, 'wb'))
+        p = subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        commit = out.strip()
+        assert p.wait() == 0
+    return commit
+
 def setup():
+    """
+    We set up a test setup with three directories, 'user', 'base1' and 'base2'.
+    'base2' is made into a git repository which is checked out. 
+    
+    """
     global tmpdir, base1, base2, user, source_cache
     tmpdir = tempfile.mkdtemp()
     base1 = pjoin(tmpdir, 'base1')
@@ -30,31 +46,32 @@ def setup():
     dump(pjoin(base1, "conflicting.txt"), "in base1")
     dump(pjoin(base2, "conflicting.txt"), "in base2")
 
-    # base2 is stored in a git repo
+    # base2 is stored in a git repo; linux.yaml refers to common.yaml so that
+    # we have two profiles with the same repo, which should not be deleted twice...
     dump(pjoin(base2, "profiles", "linux.yaml"), """\
-        parameters:
-          global:
-            d: 4
+        include:
+          - profile: common.yaml  # tests: resolution relative to current file, and refcount of tempdirs
         packages:
           - numpy/host # changed to numpy/latest in user/profile.yaml
           - python/host # not changed
     """)
-    with working_directory(base2):
-        subprocess.check_call(['git', 'init'], stdout=open(os.devnull, 'wb'))
-        subprocess.check_call(['git', 'add', 'profiles/linux.yaml', 'base2.txt', 'conflicting.txt'],
-                              stdout=open(os.devnull, 'wb'))
-        subprocess.check_call(['git', 'commit', '-m', 'Initial commit'], stdout=open(os.devnull, 'wb'))
-        p = subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = p.communicate()
-        commit = out.strip()
-        assert p.wait() == 0
-    
+    dump(pjoin(base2, "profiles", "common.yaml"), """\
+        parameters:
+          global:
+            d: 4
+    """)
+    commit = gitify(base2)
 
     dump(pjoin(user, "profile.yaml"), """\
         extends:
           - profile: profiles/linux.yaml
             dir: %(base1)s
+
+          # Git import; define both profile, package_dir and base_dir relative to root of
+          # git repo given
           - profile: profiles/linux.yaml
+            package_dir: pkgs
+            base_dir: base
             urls: [%(base2)s]
             key: git:%(commit)s
 
@@ -87,29 +104,96 @@ def teardown():
     shutil.rmtree(tmpdir)
 
 
-def test_git_loading():
-    with profile.load_profile(source_cache, {"dir": user, "profile": "profile.yaml"}) as p:
-        git_temp_unpacked_to = p.extends[1].basedir
-        assert os.path.exists(git_temp_unpacked_to)
-    assert not os.path.exists(git_temp_unpacked_to)
+@temp_working_dir_fixture
+def test_git_loading(d):
+    dump(pjoin(d, 'gitrepo', 'in_parent_dir.yaml'), """\
+    """)
 
-def test_profile_parameters():
-    with profile.load_profile(source_cache, {"dir": user, "profile": "profile.yaml"}) as p:
-        yield eq_, p.parameters, {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+    dump(pjoin(d, 'gitrepo', 'subdir', 'in_sub_dir.yaml'), """\
+        include:
+          - profile: ../in_parent_dir.yaml
+    """)
 
-def test_find_file():
-    with profile.load_profile(source_cache, {"dir": user, "profile": "profile.yaml"}) as p:
-        yield eq_, pjoin(user, 'foo.txt'), p.find_file('foo.txt')
-        yield eq_, pjoin(user, 'user.txt'), p.find_file('user.txt')
-        yield eq_, pjoin(base1, 'base1.txt'), p.find_file('base1.txt')
-        yield ok_, pjoin(base2, 'base2.txt') != p.find_file('base2.txt') # from git repo
-        yield ok_, os.path.exists(p.find_file('base2.txt')) # from git repo
-        yield eq_, None, p.find_file('nonexisting.txt')
+    commit = gitify(pjoin(d, 'gitrepo'))
 
-        def conflict():
-            with assert_raises(profile.ConflictingProfilesError):
-                p.find_file('conflicting.txt')
-        yield conflict
+    dump(pjoin(d, 'user', 'profile.yaml'), """\
+        include:
+          - profile: subdir/in_sub_dir.yaml
+            urls: [%s]
+            key: git:%s
+    """ % (pjoin(d, 'gitrepo'), commit))
+
+    p = profile.load_profile(source_cache, {"profile": pjoin(d, 'user', 'profile.yaml')})
+    git_tmp = os.path.dirname(p.includes[0].includes[0].filename)
+    assert os.path.exists(git_tmp)
+    assert os.path.exists(pjoin(git_tmp, 'subdir', 'in_sub_dir.yaml'))
+    assert git_tmp != pjoin(d, 'gitrepo')
+    del p
+    assert not os.path.exists(git_tmp)
+
+@temp_working_dir_fixture
+def test_profile_parameters(d):
+    dump(pjoin(d, "profile.yaml"), """\
+        include:
+          - profile: base1.yaml
+          - profile: base2.yaml
+        parameters:
+          global:
+            a: 1
+            b: 2
+    """)
+    
+    dump("base1.yaml", """\
+        parameters:
+          global:
+            a: 0
+            c: 3
+    """)
+
+    dump("base2.yaml", """\
+        parameters:
+          global:
+            d: 4
+    """)
+    
+    p = profile.load_profile(source_cache, {"profile": "profile.yaml"})
+    yield eq_, p.parameters, {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+@temp_working_dir_fixture
+def test_find_file(d):
+    dump(pjoin(d, "level3", "profile.yaml"), """\
+        include:
+          - profile: %s/level2/profiles/profile.yaml
+    """ % d)
+
+    dump(pjoin(d, "level2", "profiles", "profile.yaml"), """\
+        include:
+          - profile: %s/level1/profile.yaml
+        packages_dir: ../pkgs
+        base_dir: ../base
+    """ % d)
+
+    dump(pjoin(d, "level1","profile.yaml"), """\
+        packages_dir: pkgs
+        base_dir: base
+    """)
+
+    dump(pjoin(d, "level2", "base", "base.txt"), "2")
+    dump(pjoin(d, "level1", "base", "base.txt"), "1")
+    dump(pjoin(d, "level1", "base", "base1.txt"), "1")
+    dump(pjoin(d, "level2", "pkgs", "foo", "foo.yaml"), "2")
+    dump(pjoin(d, "level1", "pkgs", "foo.yaml"), "1")
+    dump(pjoin(d, "level1", "pkgs", "bar.yaml"), "1")
+
+    p = profile.load_profile(source_cache, {"profile": pjoin(d, "level3", "profile.yaml")})
+    assert pjoin(d, "level2", "pkgs", "foo", "foo.yaml") == p.find_package_file("foo")
+    assert pjoin(d, "level1", "pkgs", "bar.yaml") == p.find_package_file("bar")
+    os.unlink(pjoin(d, "level2", "pkgs", "foo", "foo.yaml"))
+    assert pjoin(d, "level1", "pkgs", "foo.yaml") == p.find_package_file("foo")
+
+    assert pjoin(d, "level2", "base", "base.txt") == p.find_base_file("base.txt")
+    assert pjoin(d, "level1", "base", "base1.txt") == p.find_base_file("base1.txt")
+
 
 def test_python_path():
     with profile.load_profile(source_cache, {"dir": user, "profile": "profile.yaml"}) as p:
