cbd93c7610533b54855303551515c9bb3d24b1d1
Ondřej Čertík <ondrej.certik@gmail.com>
2013-10-31 15:04:44 -0600

    Update the test for shebang
    The test was testing the old behavior when shebang is not updated. Since we
    update it now, we need to modify the test to expect this new behavior.

--- hashdist/spec/tests/test_package.py
+++ hashdist/spec/tests/test_package.py
@@ -64,7 +64,7 @@ def test_create_build_spec():
             "commands": [
                 {"set": "BASH", "nohash_value": "/bin/bash"},
                 {"cmd": ["$BASH", "_hashdist/build.sh"]},
-                {'hit': ['build-postprocess', '--write-protect']}]},
+                {'hit': ['build-postprocess', '--shebang=multiline', '--write-protect']}]},
         "sources": [
             {"key": "git:a3c39a03e7b8e9a3321d69ff877338f99ebb4aa2", "target": "."}
             ]}
