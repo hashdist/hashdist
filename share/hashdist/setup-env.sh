4f07bd894d87c415dbd5c22c478b700404eaaf15
Ondřej Čertík <ondrej.certik@gmail.com>
2015-05-11 13:35:09 -0600

    Add a Bash wrapper script for hit

---
+++ share/hashdist/setup-env.sh
@@ -0,0 +1,26 @@
+# This file is part of Hashdist to setup Hashdist environment for bash.
+#
+# We provide a 'hit' Bash function, that checks for subcommand is called. If it
+# is "load" or "unload", then it hooks them up with Bash to do the right thing.
+# Otherwise it just forwards the arguments to the 'hit' program.
+
+function hit {
+    _hit_subcommand=$1; shift
+    _hit_args="$@"
+
+    case $_hit_subcommand in
+        "load"|"unload")
+            hit_commands="$(command hit $_hit_subcommand --print-bash-commands $_hit_args)"
+            if [ $? -eq 0 ]; then
+                # Evaluate the commands that hit supplied using Bash
+                eval "$hit_commands"
+            else
+                # If there was an error in hit, show the error messages
+                echo "$hit_commands"
+            fi
+            ;;
+        *)
+            command hit $_hit_subcommand $_hit_args
+            ;;
+    esac
+}
