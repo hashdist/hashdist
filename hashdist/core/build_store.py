85b7cc25b475701fa89bb0225bda8acb15ef9129
cekees <cekees@gmail.com>
2016-11-25 12:14:41 -0600

    switched relocation log statements from warn to debug

--- hashdist/core/build_store.py
+++ hashdist/core/build_store.py
@@ -446,8 +446,7 @@ class BuildStore(object):
                     to_b = os.path.split(os.path.split(to_b)[0])[0]
                     from_b = os.path.split(os.path.split(from_b)[0])[0]
                     if '\x7fELF' in data[:4]:
-                        self.logger.warning(os.getenv('PROFILE_BIN_DIR'))
-                        self.logger.warning(
+                        self.logger.debug(
                             'ELF contains reference  to "%s", which is being replaced with : %s' % (from_b, to_b))
                         #if 'PATCHELF' not in env:
                         #    raise Exception('PATCHELF not set (Linux relocatable packages depend on patchelf)')
@@ -466,11 +465,11 @@ class BuildStore(object):
                             d = os.path.dirname(os.path.realpath(filename))
                             new_abs_rpaths = [abs_rpath.replace(from_b,to_b) for abs_rpath in abs_rpaths]
                             new_abs_rpaths_str = ':'.join(new_abs_rpaths)
-                            self.logger.warning('Rewriting RPATH on "%s" from "%s" to "%s"' % (filename, abs_rpaths_str, new_abs_rpaths_str))
+                            self.logger.debug('Rewriting RPATH on "%s" from "%s" to "%s"' % (filename, abs_rpaths_str, new_abs_rpaths_str))
                             _check_call(self.logger, [patchelf, '--set-rpath', new_abs_rpaths_str, filename])
                     else:
                         new_data = data.replace(from_b, to_b)
-                        self.logger.warning(
+                        self.logger.debug(
                             'File contains reference  to "%s", which is being replaced with : %s' % (from_b, to_b))
                         st = os.stat(path)
                         os.chmod( path, st.st_mode | stat.S_IWRITE )
